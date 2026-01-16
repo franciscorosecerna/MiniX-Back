88bd30a4c00a793cb8e2fbf9632d0feeece63811
fede <federico.nicolas.polidoro@gmail.com>
2026-01-15 22:19:18 -0300

    fix: faltaba * al final del regex

--- DTOs/ChangePasswordRequest.cs
+++ DTOs/ChangePasswordRequest.cs
@@ -10,7 +10,7 @@ namespace MiniX.Backend.DTOs
 
         [Required(ErrorMessage = "La nueva contraseña es requerida")]
         [StringLength(100, MinimumLength = 8, ErrorMessage = "La contraseña debe tener entre 8 y 100 caracteres")]
-        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]",
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]*",
             ErrorMessage = "La contraseña debe contener al menos una mayúscula, una minúscula, un número y un carácter especial")]
         [DataType(DataType.Password)]
         public string NewPassword { get; set; } = string.Empty;
